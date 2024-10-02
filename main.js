ec97824f70982862fd85fa3d524829bb3ef96cf4
MuseumIT2024 <b1022167@fun.ac.jp>
2024-10-02 16:12:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -19,5 +19,51 @@ const quiz = [
  ],
  correct: '伊地知虹夏'
  },
-
   ]
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
+ 
+const button = document.getElementsByTagName('button');
+const buttonLength = button.length;
+ 
+const setupQuiz = () => {
+ document.getElementById('question').textContent = quiz[quizIndex].question;
+ let buttonIndex = 0;
+ while(buttonIndex < buttonLength) {
+　　　　 button[buttonIndex].textContent = quiz[quizIndex].choices[buttonIndex];
+　　　　 buttonIndex++;
+ }
+}
+ 
+setupQuiz();
+
+const clickHandler = (e) => {
+ if (quiz[quizIndex].correct === e.target.textContent) {
+   window.alert("正解！");
+   score++;
+ } else {
+   window.alert("不正解！");
+ }
+ 
+ quizIndex++;
+}
+ 
+let handlerIndex = 0;
+while(handlerIndex < buttonLength) {
+ button[handlerIndex].addEventListener('click', (e) => {
+   clickHandler(e);
+ });
+ handlerIndex++;
+}
+
+const clickHandler = (e) => {
+  ...
+  quizIndex++;
+  if (quizIndex < quizLength) {
+    setupQuiz();
+ } else {
+  window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！');
+ }
+}
