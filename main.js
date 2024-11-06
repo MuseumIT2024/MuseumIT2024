9c5f6e36b359a177d7e6390b90907ed2300d41c0
MuseumIT2024 <b1022167@fun.ac.jp>
2024-11-06 17:38:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ const quiz = [
             '札幌在住の人'
         ],
         correct: '函館在住の人',
-        hint:'情報ライブラリーは教職員、地域の人が利用可能です'
+        hint:'情報ライブラリーは教職員、地域の人が利用可能です',
+        tip:'未来大学の学生、教職員、函館市、北斗市、七飯町に在住・在勤・在学している方は、本を借りることができます'
     },
     {
-        question: '情報ライブラリーの自習スペースの机の天板は何で作られていますか？',
+        question: '自習スペースの机の天板は何で作られていますか？',
         choices: [
             '鉄板',
             '木',
@@ -19,7 +20,8 @@ const quiz = [
             '３Dプリンター'
         ],
         correct: '木',
-        hint:'小学校から馴染みがある人が多いと思います。'
+        hint:'小学校から馴染みがある人が多いと思います。',
+        tip:'机の天板は木で作られています'
     },
     {
         question: '情報ライブラリー独自のこだわりはなんですか？',
@@ -31,7 +33,8 @@ const quiz = [
             '帯を単語帳のようにまとめ、自由に閲覧できるようにしている'
         ],
         correct: '帯を切り取り表紙の裏に貼ることで、帯の内容を残している',
-        hint:'帯を捨てるということがなくなります'
+        hint:'帯を捨てるということがなくなります',
+        tip:'帯を切り取り表紙の裏にはることで帯の内容を残している。透明なカバーで本を覆っていたり、情報ライブラリー独自でのこだわりもあるので実際に見てみてね'
     },
     {
         question: 'ライブラリーの奥の方に４つの大きなオブジェがあります。どんな特徴をしていますか？',
@@ -42,7 +45,8 @@ const quiz = [
             '全て違う'
         ],
         correct: '全て違う',
-        hint:'模型の〇番をみてみよう！'
+        hint:'模型の〇番をみてみよう！',
+        tip:'よく見てね、4つとも違う形をしているよ'
     },
     {
         question: '情報ライブラリーには様々な特徴のある家具がある。1つは寝転んで本が読めるような家具。2つ目は何ですか？',
@@ -51,10 +55,10 @@ const quiz = [
             '貴族のような家具',
             '近未来な家具',
             '未来大学のような家具'
-
         ],
         correct: 'バーカウンターのような家具',
-        hint:'この家具はリラックスした雰囲気です'
+        hint:'この家具はリラックスした雰囲気です',
+        tip:'自由な空間を補強するようなものとしてこれらの家具になりました'
     },
     {
         question: '食堂は何階にありますか？',
@@ -63,10 +67,10 @@ const quiz = [
             '１階',
             '地下２階',
             '食堂なんてものはない'
-
         ],
         correct: '１階',
-        hint:'大学に来てたらわかりますよね'
+        hint:'大学に来てたらわかりますよね',
+        tip:'クイズ６の解説です'
     },
 ]
 
@@ -88,6 +92,10 @@ v.pause();
 
 
 const setupQuiz = () => {
+    document.getElementById('kotae').style.display = 'none';
+    document.getElementById('answer').style.display = 'none';
+    document.getElementById('tip').style.display = 'none';
+    document.getElementById('tip').textContent = quiz[quizIndex].tip;
     document.getElementById('quizno').textContent = 'Q.' + (quizIndex+1);
     document.getElementById('question').textContent = quiz[quizIndex].question;
     let buttonIndex = 0;
@@ -97,28 +105,38 @@ const setupQuiz = () => {
     }
     document.getElementById('hint').textContent = '';
     document.getElementById('hintphoto').style.display = 'none'
-}
 
+    if (quizIndex == quizLength-1) {
+        document.getElementById('nextbutton').textContent = '終了する'
+    }
+
+}
+let moveflag = 1;
 setupQuiz();
 
 const clickHandler = (e) => {
-    if (quiz[quizIndex].correct === e.target.textContent) {
-        window.alert("正解！");
-        score++;
-    } else {
-        window.alert("不正解！");
-    }
 
-    quizIndex++;
+    document.getElementById('kotae').style.display = 'block';
+    document.getElementById('answer').textContent = quiz[quizIndex].correct;
+    document.getElementById('answer').style.display = 'block';
+    document.getElementById('button').style.display = 'none'
+    document.getElementById('nextbutton').style.display = 'block';
+    document.getElementById('hintbutton').style.display = 'none';
+    document.getElementById('hintphoto').style.display = 'none'
+    document.getElementById('hint').style.display = 'none'
+    document.getElementById('tip').style.display = 'block';
+    if(moveflag == 1){
+        if (quiz[quizIndex].correct === e.target.textContent) {
+            window.alert("正解！");
+            score++;
+            moveflag = 0
+        } else {
+            window.alert("不正解！");
+            moveflag = 0
+        }
+    }
      //document.getElementById('hintphoto').src=hintp[quizIndex];
 
-     function hideQuiz(){ 
-        document.getElementById('card').style.display = 'none';
-        document.getElementById('card').style.height = '58vh';
-        document.getElementById('card').style.height = '58svh';
-        document.getElementById('hintbutton').style.display = 'none';
-        document.getElementById('hintphoto').style.display = 'none';
-     }
 
 
     function showQuiz(){
@@ -128,11 +146,40 @@ const clickHandler = (e) => {
     }
 
     v.addEventListener("ended", (event) =>{
-     showQuiz();
+        showQuiz();
+   
+       });
 
+}
+
+let handlerIndex = 0;
+while(handlerIndex < buttonLength) {
+    button[handlerIndex].addEventListener('click', (e) => {
+        clickHandler(e);
     });
+    handlerIndex++;
+}
 
+function showHint(){
+    if (document.getElementById('hint').textContent ==''){
+        document.getElementById('hint').textContent = quiz[quizIndex].hint;
+        document.getElementById('hint').style.display = 'block'
+        document.getElementById('hintphoto').style.display = 'block';
+    }else{
+        document.getElementById('hint').textContent ='';
+        document.getElementById('hint').style.display = 'none'
+        document.getElementById('hintphoto').style.display = 'none';
+    }
 
+}
+
+function nextQuiz(){
+    moveflag = 1
+    document.getElementById('button').style.display = 'block'
+    quizIndex++;
+    document.getElementById('nextbutton').style.display = 'none';
+    document.getElementById('hintbutton').style.display = 'block';
+    document.getElementById('tip').style.display = 'none';
 
     if (quizIndex < quizLength) {
         document.getElementById('hintphoto').src = 'hint'+(quizIndex+1)+ '.png';
@@ -153,27 +200,14 @@ const clickHandler = (e) => {
         document.getElementById('hintbutton').style.display = 'none';
         document.getElementById('hintphoto').style.display = 'none';
         document.getElementById('rankingbutton').style.display = 'block';
-
         //document.getElementById('quizscore').textContent = 'sasaas';
     }
-}
 
-let handlerIndex = 0;
-while(handlerIndex < buttonLength) {
-    button[handlerIndex].addEventListener('click', (e) => {
-        clickHandler(e);
-    });
-    handlerIndex++;
-}
-
-function showHint(){
-    if (document.getElementById('hint').textContent ==''){
-        document.getElementById('hint').textContent = quiz[quizIndex].hint;
-        document.getElementById('hintphoto').style.display = 'block';
-    }else{
-        document.getElementById('hint').textContent ='';
+    function hideQuiz(){ 
+        document.getElementById('card').style.display = 'none';
+        document.getElementById('card').style.height = '58vh';
+        document.getElementById('card').style.height = '58svh';
+        document.getElementById('hintbutton').style.display = 'none';
         document.getElementById('hintphoto').style.display = 'none';
-    }
-
+     }
 }
-
