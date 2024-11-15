04e600fc202907ab9c7c6f78c0b20a88bcc856af
MuseumIT2024 <b1022167@fun.ac.jp>
2024-11-15 15:07:26 +0900

    Update rankmain.js

--- rankmain.js
+++ rankmain.js
@@ -2,9 +2,9 @@
 var scor = document.createElement("script");
 scor.src='main.js';
 
-import { score } from './main.mjs';
+//import { score } from './main.mjs';
 
 //let sco = new score;
 var sco = 0;
 
-    document.getElementById('quizscore').textContent = score;
+  //  document.getElementById('quizscore').textContent = score;
