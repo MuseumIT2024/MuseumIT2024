cbf13aa9b6e9286ec50bad534d2bd553a35a269b
MuseumIT2024 <b1022167@fun.ac.jp>
2024-11-15 14:24:51 +0900

    Create rankmain.js

---
+++ rankmain.js
@@ -0,0 +1,10 @@
+
+var scor = document.createElement("script");
+scor.src='main.js';
+
+import { score } from './main.mjs';
+
+//let sco = new score;
+var sco = 0;
+
+    document.getElementById('quizscore').textContent = score;
